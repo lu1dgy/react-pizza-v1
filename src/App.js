e48fb7306e2ac337a30271be2cf27e6b7aeaa769
alex <lapkesa@gmail.com>
2022-12-03 01:07:59 +0200

    new:add PropTypes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import { useEffect, useState } from 'react';
+import axios from 'axios';
 import { Route, Routes } from 'react-router-dom';
 import { Header } from './components/index';
 import { Home, Cart } from './pages';
 
 function App() {
-  const [state, setPizzas] = useState();
+  const [pizzas, setPizzas] = useState([]);
 
-  useEffect(() => {}, []);
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/db.json')
+      .then(({ data }) => setPizzas(data.pizzas));
+  }, []);
 
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <Routes>
-          <Route exact path="/" element={<Home />} />
+          <Route exact path="/" element={<Home items={pizzas} />} />
           <Route exact path="/cart" element={<Cart />} />
         </Routes>
       </div>
